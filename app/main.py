71479d0d863e87ab9beea663817e8b58dcd75f4e
Dolgov Denis <dolgovdenis35@gmail.com>
2025-05-12 21:02:46 +0300

    add logging in main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,27 @@
 import uvicorn
 from fastapi import FastAPI
+from loguru import logger
 
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root() -> dict[str, str]:
+    logger.debug("root page logic...")
     return {"message": "This is a root page"}
 
 
 if __name__ == "__main__":
+    logger.add(
+        "logs/logbook.log",
+        format="{time} {level} {module}:{line} {message}",
+        rotation="08:00",
+        compression="zip",
+        enqueue=True,
+    )
+    logger.debug("The logger has been configured")
+
+    logger.debug("Uvicorn gets to work...")
     uvicorn.run("app.main:app")
+    logger.debug("Uvicorn got to work!")
